0b887eaed4218dea85407e15de2bf44e5c48c769
kimocoder <christian@aircrack-ng.org>
2024-10-11 16:05:00 +0000

    Update dr5018 support

--- target/linux/qualcommax/image/ipq50xx.mk
+++ target/linux/qualcommax/image/ipq50xx.mk
@@ -1,3 +1,9 @@
+define Device/EmmcImage
+	IMAGES += factory.bin sysupgrade.bin
+	IMAGE/factory.bin := append-rootfs | pad-rootfs | pad-to 64k
+	IMAGE/sysupgrade.bin/squashfs := append-rootfs | pad-to 64k | sysupgrade-tar rootfs=$$$$@ | append-metadata
+endef
+
 define Device/redmi_ax3000
   $(call Device/FitImage)
   $(call Device/UbiFit)
@@ -18,10 +24,16 @@ endef
 TARGET_DEVICES += redmi_ax3000
 
 define Device/wallys_dr5018
-  DEVICE_TITLE := Wallys DR5018
-  DEVICE_DTS := ipq5018-mp03.5-c1
-  SUPPORTED_DEVICES := wallys,dr5018
-  DEVICE_PACKAGES := ath11k-wifi-wallys-dr5018 uboot-envtools ath11k-firmware-ipq50xx-spruce ath11k-firmware-qcn6122
+  $(call Device/FitImage)
+  $(call Device/EmmcImage)
+  SOC := ipq5018
+  DEVICE_VENDOR := Wallys
+  DEVICE_MODEL := DR5018
+  BLOCKSIZE := 64k
+  PAGESIZE := 2048
+  KERNEL_SIZE := 6144k
   DEVICE_DTS_CONFIG := config@mp03.5-c1
+  DEVICE_PACKAGES := ipq-wifi-wallys_dr5018
+  IMAGE/factory.bin := append-kernel | pad-to $${KERNEL_SIZE}  |  append-rootfs | append-metadata
 endef
 TARGET_DEVICES += wallys_dr5018
